29bacdce783bec763ba785eaa7f6e8e4599cc8c4
SNIGDHA2101 <61613730+SNIGDHA2101@users.noreply.github.com>
2020-02-29 15:53:56 +0530

    changed variable to x

--- sub.c
+++ sub.c
@@ -1,14 +1,14 @@
 #include <iostream>
 int main() {    
 
-    int number1, number2, diff;
+    int number1, number2, x;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
-    diff = number1 - number2;      
+    x = number1 - number2;      
     
-    printf("%d - %d = %d", number1, number2, diff);
+    printf("%d - %d = %d", number1, number2, x);
     return 0;
 }
